6e6c38981e2a8cb6c210ec4f7abe47f5fa934cea
nadehamz <nadehamz@sheridancollege.ca>
2024-02-23 16:23:52 -0500

    hamazh_desgin commit

---
+++ src/hamzah_Design.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class hamzah_Design {
+    // I made 2 friends in this class 
+    // this classs happens on fridays from 3pm-6pm
+}
