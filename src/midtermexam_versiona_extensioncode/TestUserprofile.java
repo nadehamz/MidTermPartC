78def5760a5310fe2c86f0ee5e6d3badfd854ce8
nadehamz <nadehamz@sheridancollege.ca>
2024-02-23 16:16:52 -0500

    testuserprofile class commit

--- src/midtermexam_versiona_extensioncode/TestUserprofile.java
+++ src/midtermexam_versiona_extensioncode/TestUserprofile.java
@@ -3,11 +3,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package midtermexam_versiona_extensioncode;
-
+import java.util.Scanner;
 /**
  *
  * @author User
  */
 public class TestUserprofile {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String userName = scanner.nextLine();
+
+        System.out.print("Enter your favorite actor: ");
+        String favoriteActor = scanner.nextLine();
+
+        System.out.println("User Profile created for " + userName + " with favorite actor " + favoriteActor);
+
+        scanner.close();
+    }
 }
